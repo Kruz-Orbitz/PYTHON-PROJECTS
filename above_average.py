9a18601fc265926d3868b3212215c693080cf764
kruz-orbitz <kingsley2k4@gmail.com>
2026-09-08 11:38:23 +0100

    using while loop and range to print numbers

--- above_average.py
+++ above_average.py
@@ -1,9 +1,11 @@
 """
-Write a program using a while loop that prints only the even numbers from 2
+Write a function called count_down() that uses a while 
+loop to print the numbers from 5 down to 1.
 """
+def count_down(start, end):
+    number = 5
+    while number >= 1:
+        print(number)
+        number = number - 1
 
-number = 10
-
-while number >= 1:
-    print(number)
-    number = number - 1
+(count_down(5, 1))
