f3161a6b3387ccd26bc71e672bf1b327b0e3d264
kruz-orbitz <kingsley2k4@gmail.com>
2026-09-08 11:50:20 +0100

    Save progress on above average script

--- above_average.py
+++ above_average.py
@@ -2,10 +2,10 @@
 Write a function called count_down() that uses a while 
 loop to print the numbers from 5 down to 1.
 """
-def count_down(start, end):
+def count_down():
     number = 5
     while number >= 1:
         print(number)
         number = number - 1
 
-(count_down(5, 1))
+count_down()
