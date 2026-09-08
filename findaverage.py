40e607c6b26728e3881179cff4e10c6166b1fcca
kruz-orbitz <kingsley2k4@gmail.com>
2026-09-08 16:57:52 +0100

    findaverage

---
+++ findaverage.py
@@ -0,0 +1,7 @@
+def find_remainder(number1, number2, number3):
+    average = (number1 + number2 + number3) / 3
+
+    return average
+
+
+print(find_remainder(20, 15, 30))
--- findremainder.py
+++
@@ -1,7 +0,0 @@
-def find_remainder(number1, number2):
-    remainder = number1 % number2
-
-    return remainder
-
-
-print(find_remainder(25, 10))
